074103ffcea3eb54152b8d92e18b96e1d761c4af
Hippolyte Massicot <hippolyte.massicot@stef.com>
2025-06-09 16:24:02 +0100

    fix: restore original query database

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,11 +1,15 @@
 import postgres from 'postgres';
 
+
+
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 async function listInvoices() {
 	const data = await sql`
-    SELECT invoices.amount
+    SELECT invoices.amount, customers.name
     FROM invoices
+    JOIN customers ON invoices.customer_id = customers.id
+    WHERE invoices.amount = 666;
   `;
 
 	return data;
